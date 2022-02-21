efa67124f7ae1176817c3a684c6e552c202739c8
瞿贵友 <quguiyou1076@qq.com>
2022-02-21 22:08:28 +0800

    实现 proxyRefs 功能

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -48,7 +48,36 @@ export const isRef = (ref: any): ref is Ref<unknown> => {
   return ref?.['__v_isRef'] ?? false
 }
 
+export type ShallowUnwrapRef<T> = {
+  [K in keyof T]: T[K] extends Ref<infer V>
+    ? V
+    : // if `V` is `unknown` that means it does not extend `Ref` and is undefined
+    T[K] extends Ref<infer V> | undefined
+    ? unknown extends V
+      ? undefined
+      : V | undefined
+    : T[K]
+}
+
 export const unRef = <T>(ref: Ref<T>): T => {
   if (isRef(ref)) return ref.value
   else return ref
+}
+
+export const proxyRefs = <T extends object>(
+  objectWithRefs: T
+): ShallowUnwrapRef<T> => {
+  return new Proxy(objectWithRefs, {
+    get(target, key) {
+      return unRef(Reflect.get(target, key))
+    },
+    set(target: any, key, value) {
+      if (isRef(target[key]) && !isRef(value)) {
+        target[key].value = value
+        return true
+      } else {
+        return Reflect.set(target, key, value)
+      }
+    },
+  }) as any
 }
